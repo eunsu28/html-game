3480191baa44357b0f85ceb310c17c356f098b18
eunsu28 <ayay.e2008@gmail.com>
2021-08-31 13:55:47 +0800

    random position

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-30;
+var x = Math.floor(Math.random() * (480 - 1 + 1)) + 1;
+var y = Math.floor(Math.random() * (320 - 1 + 1)) + 1;
 const button = document.querySelector("#button");
 const ballRadius = 10;
 let dy = -10;
@@ -45,35 +45,3 @@ function a() {
 drawBall();
 button.addEventListener("click", a);
 
-// var canvas = document.getElementById("myCanvas");
-// var ctx = canvas.getContext("2d");
-// var ballRadius = 10;
-// var x = canvas.width/2;
-// var y = canvas.height-30;
-// var dx = 2;
-// var dy = -2;
-
-// function drawBall() {
-//     ctx.beginPath();
-//     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-//     ctx.fillStyle = "#0095DD";
-//     ctx.fill();
-//     ctx.closePath();
-// }
-
-// function draw() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     drawBall();
-    
-//     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-//         dx = -dx;
-//     }
-//     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-//         dy = -dy;
-//     }
-    
-//     x += dx;
-//     y += dy;
-// }
-
-// setInterval(draw, 10);
