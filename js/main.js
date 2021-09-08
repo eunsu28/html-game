e64ce74375412746d3b617e85c0493a1d98c8460
eunsu28 <ayay.e2008@gmail.com>
2021-09-08 20:12:47 +0800

    ㅇㅡ악!!!!!
    아니 버튼을 없애야 하나?

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ var rightPressed = false;
 var leftPressed = false;
 //
 
-//event listener
+//add event listenr
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 //
@@ -72,23 +72,21 @@ function draw() { //지우고 그리고 지우고 그리고
     drawBall();
     drawPaddle();
     
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy < ballRadius) {
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
-    else if(y + dy > canvas.height-ballRadius) {
-        if(x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy;
-        }
-        else {
-            alert("GAME OVER");
-            document.location.reload();
-            clearInterval(interval); // Needed for Chrome to end game
-        }
+
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        alert("GAME OVER");
+        document.location.reload();
+        clearInterval(interval); // Needed for Chrome to end game
+        
     }
-    
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 7;
     }
@@ -96,13 +94,27 @@ function draw() { //지우고 그리고 지우고 그리고
         paddleX -= 7;
     }
 
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+        }
+    }
+
+    x += dx;
+    y += dy;
 }
 
 //버튼 누르면 시작 및 새로 시작
 function a() {
     num += 1;
     if(num === 1){
-        setInterval(draw, 10); //10 millisecond 
         var interval = setInterval(draw, 10);
     } else if(num === 2){
         window.location.reload() //재시작
@@ -119,71 +131,3 @@ button.addEventListener("click", a);
 //
 
 //github.dev
-
-// function keyDownHandler(e) {
-//     if(e.key == "Right" || e.key == "ArrowRight") {
-//         rightPressed = true;
-//     }
-//     else if(e.key == "Left" || e.key == "ArrowLeft") {
-//         leftPressed = true;
-//     }
-// }
-
-// function keyUpHandler(e) {
-//     if(e.key == "Right" || e.key == "ArrowRight") {
-//         rightPressed = false;
-//     }
-//     else if(e.key == "Left" || e.key == "ArrowLeft") {
-//         leftPressed = false;
-//     }
-// }
-
-// function drawBall() {
-//     ctx.beginPath();
-//     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-//     ctx.fillStyle = "#0095DD";
-//     ctx.fill();
-//     ctx.closePath();
-// }
-// function drawPaddle() {
-//     ctx.beginPath();
-//     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-//     ctx.fillStyle = "#0095DD";
-//     ctx.fill();
-//     ctx.closePath();
-// }
-
-// function draw() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     drawBall();
-//     drawPaddle();
-    
-//     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-//         dx = -dx;
-//     }
-//     if(y + dy < ballRadius) {
-//         dy = -dy;
-//     }
-//     else if(y + dy > canvas.height-ballRadius) {
-//         if(x > paddleX && x < paddleX + paddleWidth) {
-//             dy = -dy;
-//         }
-//         else {
-//             alert("GAME OVER");
-//             document.location.reload();
-//             clearInterval(interval); // Needed for Chrome to end game
-//         }
-//     }
-    
-//     if(rightPressed && paddleX < canvas.width-paddleWidth) {
-//         paddleX += 7;
-//     }
-//     else if(leftPressed && paddleX > 0) {
-//         paddleX -= 7;
-//     }
-    
-//     x += dx;
-//     y += dy;
-// }
-
-// var interval = setInterval(draw, 10);
